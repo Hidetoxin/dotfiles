3b7b637b5eeb2e8c7bc0a76b16a8debf3fe7e316
Fernando Becerra Méndez <github@hidetox.in>
2021-09-01 22:40:17 -0500

    :fire: Remove un-necessary zsh plugins

--- dot_config/zinit/snippets.zsh
+++ dot_config/zinit/snippets.zsh
@@ -1,18 +1,21 @@
-# vim: syntax=zsh
+# vim: syntax=sh
 
 # LOAD OMZ SNIPPETS
-# zinit snippet OMZ::plugins/fd/_fd
-zinit snippet OMZ::plugins/aws/aws.plugin.zsh
-zinit snippet OMZ::plugins/fzf/fzf.plugin.zsh
-zinit snippet OMZ::plugins/tig/tig.plugin.zsh
-zinit snippet OMZ::plugins/fasd/fasd.plugin.zsh
-zinit snippet OMZ::plugins/httpie/httpie.plugin.zsh
-# zinit snippet OMZ::plugins/ripgrep/_ripgrep
-zinit snippet OMZ::plugins/vi-mode/vi-mode.plugin.zsh
+
+# auto-completion with fzf
+# zinit snippet OMZ::plugins/fzf/fzf.plugin.zsh
+
+# # vim like key bindings
+# zinit snippet OMZ::plugins/vi-mode/vi-mode.plugin.zsh
+
+# un-archive any file pressing 'x'
 zinit snippet OMZ::plugins/extract/extract.plugin.zsh
+
+# correct command pressing 'esc - esc'
 zinit snippet OMZ::plugins/thefuck/thefuck.plugin.zsh
-zinit snippet OMZ::plugins/terraform/terraform.plugin.zsh
-zinit snippet OMZ::plugins/tmuxinator/tmuxinator.plugin.zsh
-zinit snippet OMZ::plugins/fancy-ctrl-z/fancy-ctrl-z.plugin.zsh
-zinit snippet OMZ::plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
-zinit snippet OMZ::plugins/zsh-navigation-tools/zsh-navigation-tools.plugin.zsh
+
+# toggle fg and bg commands pressing 'ctrl + z'
+# zinit snippet OMZ::plugins/fancy-ctrl-z/fancy-ctrl-z.plugin.zsh
+
+# change directoy using fzf pressing 'tab' while doing cd
+# zinit snippet OMZ::plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
