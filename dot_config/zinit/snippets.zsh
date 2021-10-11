f8c4437dcf815f615db1613c3fee244c40cd3ead
Fernando Becerra Méndez <github@hidetox.in>
2021-10-10 21:12:43 -0500

    :wastebasket: Remove un-used plugins

--- dot_config/zinit/snippets.zsh
+++ dot_config/zinit/snippets.zsh
@@ -3,19 +3,20 @@
 # LOAD OMZ SNIPPETS
 
 # auto-completion with fzf
-# zinit snippet OMZ::plugins/fzf/fzf.plugin.zsh
-
-# # vim like key bindings
-# zinit snippet OMZ::plugins/vi-mode/vi-mode.plugin.zsh
+zinit ice --lucid --if="[ -n $(command -v fzf) ]" \
+    && zinit snippet "OMZ::plugins/fzf/fzf.plugin.zsh"  # after mcfly
 
 # un-archive any file pressing 'x'
-zinit snippet OMZ::plugins/extract/extract.plugin.zsh
+zinit ice --lucid \
+    && zinit snippet "OMZ::plugins/extract/extract.plugin.zsh"
 
 # correct command pressing 'esc - esc'
-zinit snippet OMZ::plugins/thefuck/thefuck.plugin.zsh
+zinit ice --lucid \
+    && zinit snippet "OMZ::plugins/thefuck/thefuck.plugin.zsh"
 
 # toggle fg and bg commands pressing 'ctrl + z'
-# zinit snippet OMZ::plugins/fancy-ctrl-z/fancy-ctrl-z.plugin.zsh
+zinit ice --lucid \
+    && zinit snippet "OMZ::plugins/fancy-ctrl-z/fancy-ctrl-z.plugin.zsh"
 
 # change directoy using fzf pressing 'tab' while doing cd
 # zinit snippet OMZ::plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
