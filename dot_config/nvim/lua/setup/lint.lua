501798b15028e1ec1ffbc9b91ede0295775496ae
Fernando Becerra Méndez <github@hidetox.in>
2023-03-07 20:52:03 -0600

    :truck: Re-name some nvim plugins
    Change the plugins names to make easier to figure form where I got them

--- dot_config/nvim/lua/setup/lint.lua
+++
@@ -1,8 +0,0 @@
--- CONFIGURATION
-local lint = require('lint')
-lint.linters_by_ft({
-  go           = {'golangcilint'},
-  markdown     = {'markdownlint'},
-  codespell    = {'codespell'},
-  ansible_lint = {'ansible-lint'.}
-})
