1ed5285721bcf065bfc94bc925fed3ef3461e508
Fernando Becerra Méndez <github@hidetox.in>
2024-10-15 16:12:20 +0900

    :recycle: Split `zinit` configuration
    Split `zinit` configuration into `plugins` and `oh-my-zsh` snippets.

--- dot_config/zinit/oh-my-zsh.zsh
+++ dot_config/zinit/oh-my-zsh.zsh
@@ -1,37 +1,9 @@
 #!/bin/sh
 
-# Add plugins 
-# Add in `powerlevel10k`
-zinit ice depth=1 --lucid \
-  && zinit light 'romkatv/powerlevel10k'
+# Add `oh-my-zsh` snippets, more information at:
+# - https://github.com/ohmyzsh/ohmyzsh/wiki/plugins
+# - https://zdharma-continuum.github.io/zinit/wiki/example-oh-my-zsh-setup/
 
-# Add `fzf` for tab completion
-zinit ice --lucid --if="[ -n $(command -v fzf) ]" \
-    && zinit light 'aloxaf/fzf-tab'               \
-    && zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-
-# Colorize output using the `grc` command
-zinit ice --lucid --wait=0 --if="[ -n $(command -v grc) ]" \
-    && zinit light 'unixorn/warhol.plugin.zsh'
-
-# Add completion
-zinit ice --lucid \
-    && zinit light 'zsh-users/zsh-completions'
-    # zicompinit; zicdreplay  # fix fzf-tab
-
-# Suggest a command based on previous history
-zinit ice --lucid \
-    && zinit light 'zsh-users/zsh-autosuggestions'
-
-# Alias reminder when you type a full command
-zinit ice --lucid \
-    && zinit light 'michaelaquilina/zsh-you-should-use'
-
-# Add syntax highlighting for the shell
-zinit ice --lucid \
-    && zinit light 'zdharma-continuum/fast-syntax-highlighting'
-
-# Add `oh-my-zsh` snippets
 # Add `go` completion and aliases
 # zinit ice --lucid --wait=0 --if="[ -n $(command -v go) ]" \
 #     && zinit snippet 'OMZ::plugins/golang/golang.plugin.zsh'
