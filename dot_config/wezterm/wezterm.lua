5603b6f2806a0b12ad076453cd9ffd6ee58cb05a
Fernando Becerra <github@hidetox.in>
2025-03-26 22:26:23 -0600

    :lipstick: Add wallpaper to `wezterm`
    Add script to create a link to my `icloud` and point `wezterm` to it to pick-up my wallpapers.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 15
 config.color_scheme = "GruvboxDark"
 
-config.window_background_image = "/Users/hdtxn/Downloads/background.jpg"
+config.window_background_image = os.getenv("HOME") .. "/Cloud/iCloud/Wallpapers/Terminal/Gruvbox/background.jpg"
 config.window_background_opacity = 0.96
 
 config.hide_tab_bar_if_only_one_tab = true
