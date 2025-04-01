78b2d49ba946025b656ae23376f7b63871fb04fd
Fernando Becerra <github@hidetox.in>
2025-03-31 21:41:11 -0600

    :sparkles: Add `precognition` plugin
    Add `neovim` plugin to help navigate by showing some `vim` motions.

--- dot_config/nvim/lua/custom/plugins/precognition.lua
+++ dot_config/nvim/lua/custom/plugins/precognition.lua
@@ -1,5 +1,31 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+
+return {
+    "tris203/precognition.nvim",
+    --event = "VeryLazy",
+    opts = {
+    startVisible = true,
+    -- showBlankVirtLine = true,
+    -- highlightColor = { link = "Comment" },
+    -- hints = {
+    --      Caret = { text = "^", prio = 2 },
+    --      Dollar = { text = "$", prio = 1 },
+    --      MatchingPair = { text = "%", prio = 5 },
+    --      Zero = { text = "0", prio = 1 },
+    --      w = { text = "w", prio = 10 },
+    --      b = { text = "b", prio = 9 },
+    --      e = { text = "e", prio = 8 },
+    --      W = { text = "W", prio = 7 },
+    --      B = { text = "B", prio = 6 },
+    --      E = { text = "E", prio = 5 },
+    -- },
+    -- gutterHints = {
+    --     G = { text = "G", prio = 10 },
+    --     gg = { text = "gg", prio = 9 },
+    --     PrevParagraph = { text = "{", prio = 8 },
+    --     NextParagraph = { text = "}", prio = 8 },
+    -- },
+    -- disabled_fts = {
+    --     "startify",
+    -- },
+    },
+}
