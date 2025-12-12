93c932c1fff1dea4caebecbdd73ce8c7af81dcda
Fernando Becerra <github@hidetox.in>
2025-12-12 11:57:39 -0600

    :bug: Fix lazygit on tmux
    Term is not correctly set on lazygit.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,11 +1,13 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
 local config = {}
 
-config.font = wezterm.font("Hack Nerd Font Mono")
-config.font_size = 15
-config.color_scheme = "GruvboxDark"
+config.term = 'wezterm'
 
-config.window_background_image = os.getenv("HOME") .. "/Cloud/iCloud/Wallpapers/Terminal/Gruvbox/background.jpg"
+config.font = wezterm.font('Hack Nerd Font Mono')
+config.font_size = 15
+config.color_scheme = 'GruvboxDark'
+
+config.window_background_image = os.getenv('HOME') .. '/Cloud/iCloud/Wallpapers/Terminal/Gruvbox/background.jpg'
 config.window_background_opacity = 0.96
 
 config.hide_tab_bar_if_only_one_tab = true
